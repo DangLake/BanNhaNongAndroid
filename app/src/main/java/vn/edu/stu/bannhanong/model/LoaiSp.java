74ac7e19d2f8068b3a46cb7e5c593889e6cc4afa
giang <vthgiang181003@gmail.com>
2024-11-09 22:46:43 +0700

    them language, method

--- app/src/main/java/vn/edu/stu/bannhanong/model/LoaiSp.java
+++ app/src/main/java/vn/edu/stu/bannhanong/model/LoaiSp.java
@@ -1,4 +1,42 @@
 package vn.edu.stu.bannhanong.model;
 
+import java.util.List;
+
 public class LoaiSp {
+    private int maloai;
+    private String tenloai;
+    private List<Sanpham> dsSP;
+
+    public LoaiSp() {
+    }
+
+    public LoaiSp(int maloai, String tenloai, List<Sanpham> dsSP) {
+        this.maloai = maloai;
+        this.tenloai = tenloai;
+        this.dsSP = dsSP;
+    }
+
+    public int getMaloai() {
+        return maloai;
+    }
+
+    public void setMaloai(int maloai) {
+        this.maloai = maloai;
+    }
+
+    public String getTenloai() {
+        return tenloai;
+    }
+
+    public void setTenloai(String tenloai) {
+        this.tenloai = tenloai;
+    }
+
+    public List<Sanpham> getDsSP() {
+        return dsSP;
+    }
+
+    public void setDsSP(List<Sanpham> dsSP) {
+        this.dsSP = dsSP;
+    }
 }
--- app/src/main/java/vn/edu/stu/bannhanong/model/Sanpham.java
+++ app/src/main/java/vn/edu/stu/bannhanong/model/Sanpham.java
@@ -1,4 +1,71 @@
 package vn.edu.stu.bannhanong.model;
 
 public class Sanpham {
+    private int ma;
+    private String ten;
+    private String anh;
+    private double gia;
+    private String mota;
+
+    private LoaiSp loaiSp;
+
+    public Sanpham() {
+    }
+
+    public Sanpham(int ma, String ten, String anh, double gia, String mota, LoaiSp loaiSp) {
+        this.ma = ma;
+        this.ten = ten;
+        this.anh = anh;
+        this.gia = gia;
+        this.mota = mota;
+        this.loaiSp = loaiSp;
+    }
+
+    public int getMa() {
+        return ma;
+    }
+
+    public void setMa(int ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getAnh() {
+        return anh;
+    }
+
+    public void setAnh(String anh) {
+        this.anh = anh;
+    }
+
+    public double getGia() {
+        return gia;
+    }
+
+    public void setGia(double gia) {
+        this.gia = gia;
+    }
+
+    public String getMota() {
+        return mota;
+    }
+
+    public void setMota(String mota) {
+        this.mota = mota;
+    }
+
+    public LoaiSp getLoaiSp() {
+        return loaiSp;
+    }
+
+    public void setLoaiSp(LoaiSp loaiSp) {
+        this.loaiSp = loaiSp;
+    }
 }
