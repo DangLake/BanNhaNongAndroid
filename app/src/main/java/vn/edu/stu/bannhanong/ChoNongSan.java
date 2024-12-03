c9ef52c38693fc7ceedc4024de8aa7d1c2bb6994
giang <vthgiang181003@gmail.com>
2024-12-04 00:17:30 +0700

    xong navbar

--- app/src/main/java/vn/edu/stu/bannhanong/ChoNongSan.java
+++ app/src/main/java/vn/edu/stu/bannhanong/ChoNongSan.java
@@ -1,6 +1,7 @@
 package vn.edu.stu.bannhanong;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,9 @@ public class ChoNongSan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cho_nong_san);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
