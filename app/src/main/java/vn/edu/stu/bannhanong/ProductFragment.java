24173e133ff5ff3f5db6db3933b92f99d817c6b4
giang <vthgiang181003@gmail.com>
2024-12-15 00:59:33 +0700

    them giao dien dang san pham

--- app/src/main/java/vn/edu/stu/bannhanong/ProductFragment.java
+++ app/src/main/java/vn/edu/stu/bannhanong/ProductFragment.java
@@ -1,17 +1,22 @@
 package vn.edu.stu.bannhanong;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ProductFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    Button btnPostProduct;
     private String mParam1;
     private String mParam2;
 
@@ -42,5 +47,24 @@ public class ProductFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_product, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnPostProduct=view.findViewById(R.id.btnPostProduct);
+
+
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnPostProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(), DangBanSP.class);
+                startActivity(intent);
+            }
+        });
     }
 }
